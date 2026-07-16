305b6f1a821b9203c93ff65c396be496b8c77a18
Renzo Luque <80488682+renzoluquem@users.noreply.github.com>
2026-07-16 18:06:10 -0500

    second

---
+++ EventHandler.h
@@ -0,0 +1,19 @@
+/**
+* @author Renzo Andres Luque Minaya
+ */
+#ifndef EVENT_HANDLER_H
+#define EVENT_HANDLER_H
+
+class Event {
+public:
+  int id;
+  explicit Event(int id) : id(id) {}
+  bool operator==(const Event& other) const { return id == other.id; }
+};
+
+class EventHandler {
+public:
+  virtual void on(Event event) = 0;
+  virtual ~EventHandler() = default;
+};
+#endif
